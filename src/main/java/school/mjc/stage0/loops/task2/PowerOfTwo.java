f161afe1a5d7a7c505f934eaf8803860d403972f
niluwka0403 <ninimamatova0403@gmail.com>
2024-01-29 23:56:26 +0500

    WordsBuilder

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        int result = 1;
+        if (power < 0) {
+            System.out.println("too much power");
+        } else if (power == 0) {
+            System.out.println("1");
+        }else {
+            System.out.println(1);
+            for (int i = 1; i <= power; i++) {
+            result = result * 2;
+            System.out.println(result);
+        }
+        }
+
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,9 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+        for (int i = 0; i < chars.length; i++) {
+            System.out.print(chars[i]);
+        }
 
     }
 }
